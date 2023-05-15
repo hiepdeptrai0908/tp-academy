61d0d9e84550ecda9477a72d74a113bead53e57e
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2023-05-15 13:10:16 +0700

    thêm sidebar phần tuyển sinh

--- src/pages/Addmission/Addmission.jsx
+++ src/pages/Addmission/Addmission.jsx
@@ -1,5 +1,25 @@
+import classNames from 'classnames/bind'
+import styles from './Addmission.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+const cx = classNames.bind(styles)
+
 function Addmission() {
-    return ( <h1>Addmission Page</h1> );
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('sidebar')}>
+                <div className={cx('category')}>Danh mục</div>
+                <div className={cx('category-item', 'active')}>Chương trình học bổng</div>
+                <div className={cx('category-item')}>Du học tự túc</div>
+                <div className={cx('category-item')}>Kỹ năng đặc định (特定技能)</div>
+                <div className={cx('category-item')}>Du học Hàn Quốc</div>
+                <div className={cx('category-item')}>Du học Đức</div>
+                <div className={cx('category-item')}>Du học Canada</div>
+            </div>
+            <div className={cx('contents')}>Nội dung</div>
+        </div>
+    )
 }
 
-export default Addmission;
+export default Addmission
